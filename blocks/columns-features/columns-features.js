3d9ddd58d8983d1c318977659417e9ae1d04de04
Surendra Pratap Singh <surendrapras@adobe.com>
2025-12-22 13:45:28 +0530

    code changes for css footer

--- blocks/columns-features/columns-features.js
+++ blocks/columns-features/columns-features.js
@@ -1,19 +1,15 @@
 export default function decorate(block) {
-const cols = [...block.firstElementChild.children];
-block.classList.add(`columns-features-${cols.length}-cols`);
+  const cols = [...block.firstElementChild.children];
+  block.classList.add(`columns-features-${cols.length}-cols`);
 
-// setup image columns
-[...block.children].forEach((row) => {
-[...row.children].forEach((col) => {
-const pic = col.querySelector('picture');
-if (pic) {
-const picWrapper = pic.closest('div');
-if (picWrapper && picWrapper.children.length === 1) {
-picWrapper.classList.add('columns-features-img-col');
-}
-}
-});
-});
-}
-
-
+  // setup image columns
+  [...block.children].forEach((row) => {
+    [...row.children].forEach((col) => {
+      const pic = col.querySelector('picture');
+      if (pic) {
+        const picWrapper = pic.closest('div');
+        if (picWrapper && picWrapper.children.length === 1) {
+          picWrapper.classList.add('columns-features-img-col');
+        }
+      }
+    });
