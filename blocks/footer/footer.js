2d1d629c07433d835f143140c93eb76b159ba748
Surendra Pratap Singh <surendrapras@adobe.com>
2025-12-22 09:26:06 +0530

    code changes for css footer

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -14,7 +14,39 @@ export default async function decorate(block) {
   // decorate footer DOM
   block.textContent = '';
   const footer = document.createElement('div');
-  while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+
+  // Fragment has 2 divs: first with 5 columns content, second with bottom section
+  const firstDiv = fragment.children[0];
+  const secondDiv = fragment.children[1];
+
+  if (firstDiv) {
+    // Restructure first div to have each column (h2 + ul/p) in its own div
+    const columnsContainer = document.createElement('div');
+    const elements = Array.from(firstDiv.children);
+
+    let currentColumn = null;
+    elements.forEach((el) => {
+      if (el.tagName === 'H2') {
+        // Start new column
+        if (currentColumn) columnsContainer.appendChild(currentColumn);
+        currentColumn = document.createElement('div');
+        currentColumn.appendChild(el);
+      } else if (currentColumn) {
+        // Add to current column
+        currentColumn.appendChild(el);
+      }
+    });
+
+    // Add last column
+    if (currentColumn) columnsContainer.appendChild(currentColumn);
+
+    footer.appendChild(columnsContainer);
+  }
+
+  // Add second div (logo, copyright, social) as-is
+  if (secondDiv) {
+    footer.appendChild(secondDiv);
+  }
 
   block.append(footer);
 }
