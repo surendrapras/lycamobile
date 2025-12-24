120806c0f236de31bbf6effe48771e8ecffc74d7
jeyyy10 <jay@algoed.co>
2025-12-24 17:06:29 +0530

    fix format issue

--- blocks/home-cards-pricing/home-cards-pricing.js
+++ blocks/home-cards-pricing/home-cards-pricing.js
@@ -1,42 +1,42 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-    const ul = document.createElement('ul');
+  const ul = document.createElement('ul');
 
-    [...block.children].forEach((row) => {
-        const li = document.createElement('li');
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
 
-        while (row.firstElementChild) {
-            li.append(row.firstElementChild);
-        }
+    while (row.firstElementChild) {
+      li.append(row.firstElementChild);
+    }
 
-        [...li.children].forEach((div) => {
-            // Image block
-            if (div.children.length === 1 && div.querySelector('picture')) {
-                div.className = 'home-cards-pricing-card-image';
-                return;
-            }
+    [...li.children].forEach((div) => {
+      // Image block
+      if (div.children.length === 1 && div.querySelector('picture')) {
+        div.className = 'home-cards-pricing-card-image';
+        return;
+      }
 
-            // Remove empty blocks
-            if (!div.textContent.trim()) {
-                div.remove();
-                return;
-            }
+      // Remove empty blocks
+      if (!div.textContent.trim()) {
+        div.remove();
+        return;
+      }
 
-            div.className = 'home-cards-pricing-card-body';
-        });
-
-        if (li.children.length) {
-            ul.append(li);
-        }
+      div.className = 'home-cards-pricing-card-body';
     });
 
-    // Optimize images
-    ul.querySelectorAll('picture > img').forEach((img) => {
-        img.closest('picture').replaceWith(
-            createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]),
-        );
-    });
+    if (li.children.length) {
+      ul.append(li);
+    }
+  });
 
-    block.replaceChildren(ul);
+  // Optimize images
+  ul.querySelectorAll('picture > img').forEach((img) => {
+    img.closest('picture').replaceWith(
+      createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]),
+    );
+  });
+
+  block.replaceChildren(ul);
 }
