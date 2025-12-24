f67df442a10d7130c39758df1199cb20d83ce18d
jeyyy10 <jay@algoed.co>
2025-12-24 15:13:18 +0530

    feat: create header block with navigation and responsive menu interactions

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -117,14 +117,14 @@ export default async function decorate(block) {
 
   while (fragment.firstElementChild) nav.append(fragment.firstElementChild);
 
-  if (nav.children.length !== 3) {
+  if (nav.children.length === 0) {
     nav.innerHTML = `
       <div>
         <p><strong><a href="/">Lyca Mobile UK</a></strong></p>
       </div>
       <div>
         <ul>
-          <li><a href="/en/bundles/sim-only-deals/">Pay monthly</a></li>
+          <li><a href="/paymonthly/en/bundles/sim-only-deals/paymonthly">Pay monthly</a></li>
           <li><a href="/en/bundles/pay-as-you-go-sim-deals/">SIM only deals</a></li>
           <li><a href="/en/help-support">Help</a></li>
           <li><a href="/en/refer-a-friend">Refer a friend</a></li>
